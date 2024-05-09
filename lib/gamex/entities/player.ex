c2893891ec2b47ba16a5f84fda2afb96913bc79f
ug <gr8.distance@gmail.com>
2024-05-09 16:20:04 +0900

    掛け金の概念を追加する

--- lib/gamex/entities/player.ex
+++ lib/gamex/entities/player.ex
@@ -1,19 +1,20 @@
 defmodule Gamex.Entities.Player do
   alias Gamex.Entities.Card
-  @type t :: %__MODULE__{name: String.t(), hand: [Card.t()], score: integer}
-  defstruct name: "", hand: [], score: 0
+  @type t :: %__MODULE__{name: String.t(), hand: [Card.t()], score: integer, bet: integer}
+  defstruct name: "", hand: [], score: 0, bet: 0
 
   @spec new(String.t()) :: t()
   def new(name) do
-    new(name, [], 0)
+    new(name, [], 0, 0)
   end
 
-  @spec new(String.t(), [Card.t()], integer) :: t()
-  def new(name, hand, score) do
+  @spec new(String.t(), [Card.t()], integer, integer) :: t()
+  def new(name, hand, score, bet) do
     %__MODULE__{
       name: name,
       hand: hand,
-      score: score
+      score: score,
+      bet: bet
     }
   end
 end
